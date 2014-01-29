a3144b43e43e94a0b5abba2915b1a598227add97
Vivian <vivian.brown@gmail.com>
2014-01-28 19:15:15 -0800

    Added basic backend stuff. Now able to launch on dev appserver.

---
+++ src/Main.py
@@ -0,0 +1,34 @@
+import os
+import logging
+import json
+from google.appengine.ext import db
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class Site(db.Model):
+    """
+    Represent a CSA pick-up location
+    """
+    title = db.StringProperty()
+    lat = db.FloatProperty()
+    lng = db.FloatProperty()
+    description = db.StringProperty()
+
+class MainPage(webapp.RequestHandler):
+    """
+    Render the main landing page where users can view the map and details about CSAs.
+    """
+    def get(self):
+        template_values = {}
+        path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, template_values))
+
+application = webapp.WSGIApplication([
+    ('/', MainPage)], debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
