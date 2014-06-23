69af5bf3aaa8beb593c1fbf7af57eeb6814abb01
Vivian <vivian.brown@gmail.com>
2014-06-23 16:29:37 -0700

    Scrape from html and javascript and write to CSV

--- src/scrape/csa_scraper.py
+++ src/scrape/csa_scraper.py
@@ -4,6 +4,7 @@ import httplib2
 from bs4 import BeautifulSoup, SoupStrainer
 import re
 import csv
+import ast #used to evaluate string representation of list
 
 class Site():
 	def __init__(self, csa = None, name = None, address = None, time = None, lat = None, lng = None):
@@ -37,34 +38,39 @@ class Scraper():
 
 	def siteFromTable(self, table):
 		result = Site(self.csa)
-		result.name = table.find("b").find(text=True).strip()
 
+		result.name = table.find("b").find(text=True).strip()
 		details = table.find_all(class_="txt1_gb")
 		result.time = details[0].next_sibling.find(text=True).strip().replace("\n", " ")
 		if len(details) >= 2:
 			result.address = details[1].next_sibling.find(text=True).strip().replace("\n", " ")
-		else:
-			result.address = 'No address'
 
 		return result
 
+	def getLatLng(self, url, sites):
+		status, response = http.request(url)
+		regex = r"new Drop\((.*?)\)"
+		triples = map(ast.literal_eval, re.findall(regex, response,  re.DOTALL))
+		locations = dict(map(lambda x: (x[0], (x[1], x[2])), triples)) # Convert to a dictionary
+
+		for site in sites:
+			if site.name and site.name in locations:
+				site.lat, site.lng = locations[site.name]
+
+	def toCSV(self, sites):
+		with open("sites.csv", "wb") as csvfile:
+			siteWriter = csv.writer(csvfile, delimiter=',')
+			for site in sites:
+				siteWriter.writerow([site.csa, site.name, site.address, site.lat, site.lng])
 
 
+#happychild = Scraper('Happy Child CSA')
+#result = happychild.scrape('http://happychildcsa.csaware.com/store/csadetails.jsp')
+#drops = happychild.getLatLng('http://cw.csaware.com/store/dropsmap.jsp?id=3963', result)
+#happychild.toCSV(result)
 
-happychild = Scraper('Happy Child CSA')
-result = happychild.scrape('http://happychildcsa.csaware.com/store/csadetails.jsp')
-print result
-
-
-#status, response = http.request('http://cw.csaware.com/store/dropsmap.jsp?id=3963')
-status, response = http.request('http://cw.csaware.com/store/dropsmap.jsp?id=15')
-regex = r"new Drop\((.*?)\)"
-drops = re.findall(regex, response,  re.DOTALL)
 
 # Classes
 	# CSA with toCSV function
 	# Scraper base class - individual scrapers inherit from this\
 		# Geocode method
-
-# with open('eggs.csv', 'wb') as csvfile:
-
